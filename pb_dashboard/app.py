80e92796fe629a56295952acc0b62bc21928171e
vcslav-v <vcslav.v@gmail.com>
2021-04-14 21:29:41 +0300

    add httpauth

--- pb_dashboard/app.py
+++ pb_dashboard/app.py
@@ -1,7 +1,22 @@
 from flask import Flask
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+from os import environ
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+users = {
+    environ.get('SITE_LOGIN'): generate_password_hash(environ.get('SITE_PASS')),
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and \
+            check_password_hash(users.get(username), password):
+        return username
 
 @app.route('/')
+@auth.login_required
 def hello():
     return 'Hello'
